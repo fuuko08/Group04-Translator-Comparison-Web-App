c8b4ee7fa4a94a48dd8cd24cdc7ee278969fe369
Cory Williams <corywilliams@Corys-MacBook-Pro.local>
2022-10-03 17:25:35 -0700

    fixed HTML to work for mobile

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
-# Project01 Group04 Translation App
+# Project01 Group04 Translators Comparation 
 
 ## User Story
 ```
 AS A programmer
-I WANT to translate a text into different languages
-SO THAT I can communicate with my teammate from other countries
+I WANT to translate an English text into different languages using two different translating APIs
+SO THAT I can compare the results and get the most accurate result
 ```
 
-## Acceptain Criterias
+## Acceptance Criteria
 ```
 GIVEN a application with English text inputs
 WHEN I click on the drop down button
 THEN I can choose which language I want my text to be translated to
 WHEN I click on "translate" button
-THEN I am presented with the translated text to other languages
-WHEN I click on "more info" button
-THEN I am presented with a list of near by language schools
-WHEN I refresh the page
-THEN the page will be cleared
+THEN I am presented with the translated results to other languages using Google and DeepL api sources.
+WHEN I compare the 2 results
+THEN I am presented with the highlighted differences from DeepL to Google.
+WHEN I click on "clear" button 
+THEN all texts will be cleared
 ```
 
 ## Mock Up
+<img width="2237" alt="Screen Shot 2022-10-02 at 10 05 16 PM" src="https://user-images.githubusercontent.com/108949883/193505114-06b04b59-bc6c-420f-b704-d8330b39f327.png">
+
+## Description
+We are UCLA Bootcamp Feb 2023 Group 4. We built an app that translate English into different languages. Instead of using 1 translator source like other common app, we decided to build an app that use 2 sources, so users can compare the results and find the best result for that language. Our app use APTs from Google translate and DeepL. The app also highlight the differences in the results, so that users can easily compare them.  
+
+
+Check out our translator web app at https://fuuko08.github.io/Group04-Translator-Comparison-Web-App/
---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+jQuery(function($) {
+
+    // click translate btn to get output 
+    $("#translateBtn").click(async function() {        
+        let result = await requestTranslateServer();        
+        if(result != null ){
+            let firstString = result[0];
+            let secondString = result[1];   
+            if(result[0] != null && result[1] != null){       
+                highLight(firstString, secondString);
+            }            
+        }
+    });
+
+    //function to split the out strings into arrays
+    function highLight(firstString, secondString) {        
+        var firstStringArray = firstString.split(" ");        
+        var secondStringArray = secondString.split(" ");
+        $("#googleForm").append(firstString);    
+        var highlightPosArray = [];
+
+// function to put all the unmatched words into and array
+        jQuery.each(firstStringArray, function(indexArr2, itemArr2) {
+            if (firstStringArray.includes(secondStringArray[indexArr2])) {              
+            } else { 
+                if (secondStringArray[indexArr2] === undefined) {
+                } else {           
+                highlightPosArray.push(indexArr2);
+                console.log(highlightPosArray);
+                }
+            }
+        }); 
+
+// function to highlight the unmatched words and render it back to the textarea
+            if(highlightPosArray.length>0){
+                var finalString ="";
+                jQuery.each(highlightPosArray, function(currentIndex, currentItem){
+                    secondStringArray[currentItem] = "<strong>"+secondStringArray[currentItem]+"</strong>";
+                });
+                $('#deeplForm').empty();
+                
+                jQuery.each(secondStringArray, function(currentIndex, currentItem){
+                    finalString += currentItem + " ";
+                });
+
+                $('#deeplForm').append(finalString);
+            }        
+        };
+    
+        // click the clear btn to clear all text
+    $("#clearBtn").click(function() {
+        $("#deeplForm").empty();
+        $("#googleForm").empty();
+        $("#englishForm").val("");
+    })
+
+        // function save dropdown option to localstorage and reload
+    $(function() {
+        $("#languageDropdownList").on("change", function() {
+            localStorage.setItem("languages", this.value);
+        });
+        if(localStorage.getItem("languages")) {
+            $("#languageDropdownList").val(localStorage.getItem("languages"));
+        }       
+    });
+});
